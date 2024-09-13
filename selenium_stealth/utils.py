958544ab91cbab1bce0f8c4782838491510dc876
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-13 17:51:41 +0700

    feat: json writer

--- selenium_stealth/utils.py
+++ selenium_stealth/utils.py
@@ -1,24 +1,34 @@
 import json
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional, Union
 
 from selenium.webdriver import Chrome as Driver
 
 from .wrapper import evaluateOnNewDocument
 
 
-def read_json_file(file_path: Optional[str]):
+def with_utils(driver: Driver, **kwargs) -> None:
+    evaluateOnNewDocument(
+        driver, Path(__file__).parent.joinpath("js/utils.js").read_text()
+    )
+
+
+def read_json_file(file_path: Optional[str]) -> Union[dict, list, None]:
     """
     Reads a JSON file and returns its contents.
 
     Args:
-        file_path (str): The path to the JSON file.
+        file_path (Optional[str]): The path to the JSON file.
 
     Returns:
-        dict or list: The content of the JSON file as a Python dictionary or list.
+        Union[dict, list, None]: The content of the JSON file as a Python dictionary or list, or None if an error occurs.
     """
+    if file_path is None:
+        print("No file path provided.")
+        return None
+
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(file_path, mode="r", encoding="utf-8") as file:
             data = json.load(file)
         return data
     except FileNotFoundError:
@@ -28,8 +38,30 @@ def read_json_file(file_path: Optional[str]):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
+    return None
 
-def with_utils(driver: Driver, **kwargs) -> None:
-    evaluateOnNewDocument(
-        driver, Path(__file__).parent.joinpath("js/utils.js").read_text()
-    )
+
+def write_json_file(file_path: Optional[str], data: Any) -> None:
+    """
+    Writes data to a JSON file.
+
+    Args:
+        file_path (Optional[str]): The path to the JSON file.
+        data (Any): The data to write to the JSON file.
+
+    Returns:
+        None
+    """
+    if file_path is None:
+        print("No file path provided.")
+        return
+
+    try:
+        with open(file_path, mode="w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+    except IOError as e:
+        print(f"Error writing to file: {file_path}, {e}")
+    except TypeError as e:
+        print(f"Error serializing data to JSON: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
