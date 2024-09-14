17e6d3ab0ab542b2b15ec3a6afc92b88ca671235
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-14 07:29:10 +0700

    feat: description

--- selenium_stealth/navigator_webdriver.py
+++ selenium_stealth/navigator_webdriver.py
@@ -4,6 +4,7 @@ from selenium.webdriver import Chrome as Driver
 
 
 def navigator_webdriver(driver: Driver, **kwargs) -> None:
+    """Remove JS navigator.webdriver attribute"""
     evaluateOnNewDocument(
         driver, Path(__file__).parent.joinpath("js/navigator.webdriver.js").read_text()
     )
