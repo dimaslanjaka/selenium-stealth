c690124a64c1733a678e7e77c064000e13a5207c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-14 12:09:40 +0700

    first working bypass pixelscan.net with custom webgl data

--- example_manual.py
+++ example_manual.py
@@ -1,10 +1,12 @@
 import base64
+import json
 import math
 import os
+from pathlib import Path
 import re
 import time
 import traceback
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 from selenium import webdriver
 from selenium.webdriver import Chrome as WebDriver
@@ -13,32 +15,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 from selenium_stealth.audio_properties import audio_properties_override
+from selenium_stealth.chrome_app import chrome_app
+from selenium_stealth.chrome_runtime import chrome_runtime
+from selenium_stealth.hairline_fix import hairline_fix
+from selenium_stealth.iframe_content_window import iframe_content_window
+from selenium_stealth.media_codecs import media_codecs
+from selenium_stealth.navigator_languages import navigator_languages
+from selenium_stealth.navigator_permissions import (
+    navigator_permissions,
+    navigator_permissions2,
+)
+from selenium_stealth.navigator_plugins import navigator_plugins
+from selenium_stealth.navigator_vendor import navigator_vendor
+from selenium_stealth.navigator_webdriver import navigator_webdriver
 from selenium_stealth.preferences import Preferences
 from selenium_stealth.main import stealth2
 from selenium_stealth.ansi import AnsiFormatter
 from selenium_stealth.debug_utils import get_driver_webgl_info
 from selenium_stealth.user_agent_override import user_agent_override
-from selenium_stealth.utils import read_json_file
+from selenium_stealth.utils import read_json_file, with_utils, with_utils2
+from selenium_stealth.window_outerdimensions import window_outerdimensions
+from selenium_stealth.wrapper import evaluateOnNewDocument
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 # webdriver start
-
-options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-
-# options.add_argument("--headless")
-
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("useAutomationExtension", False)
-
-driver = None
-
-try:
-    driver = webdriver.Chrome(options=options, executable_path=r"\chromedriver.exe")
-except Exception as e:
-    print(f"fail load default driver: {e}")
 
 # load driver version from preferences
 preferences = Preferences()
@@ -47,12 +49,21 @@ fingerprint: Dict[str, Any] = read_json_file("data/fingerprint1.json")
 webgl_data = fingerprint.get("webgl_properties", {})
 audio_properties = fingerprint.get("audio_properties", {})
 
+userAgent = fingerprint.get("ua")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("start-maximized")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument(f"user-agent={userAgent}")
+driver = None
+
 if not driver:
     # selenium 4
     try:
-        userAgent = fingerprint.get("ua")
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument(f"user-agent={userAgent}")
         driver = webdriver.Chrome(
             options=chrome_options,
             service=ChromiumService(
@@ -73,17 +84,67 @@ if not driver:
         else:
             print(f"fail load driver selenium 4: {e}")
 
-if driver:
-    user_agent_override(
-        driver, fingerprint.get("ua"), fingerprint.get("lang"), "Win32", True
-    )
-    # url = "https://bot.sannysoft.com/"
-    # url = "https://sh.webmanajemen.com/webgl-information/"
-    # url = "https://pixelscan.net/"
-    # url = "https://webglreport.com/"
-    # url = "https://privacycheck.sec.lrz.de/active/fp_wg/fp_webgl.html"
-    url = "https://abrahamjuliot.github.io/creepjs/"
-    driver.get(url)
 
-    time.sleep(30)
-    driver.quit()
+if driver:
+    try:
+        with_utils2(driver)
+        chrome_app(driver)
+        # chrome_runtime(driver, False)
+        iframe_content_window(driver)
+        # media_codecs(driver)
+        navigator_languages(driver, fingerprint.get("lang"))
+        # navigator_permissions2(driver)
+        navigator_plugins(driver)
+        # navigator_vendor(driver, "Google Inc.")
+        navigator_webdriver(driver)
+        window_outerdimensions(driver)
+        user_agent_override(
+            driver, fingerprint.get("ua"), fingerprint.get("lang"), "Win32", True
+        )
+        js_code = """
+const getParameterProxyHandler = {
+apply: function(target, ctx, args) {
+    const param = (args || [])[0]
+    const result = utils.cache.Reflect.apply(target, ctx, args)
+    // UNMASKED_VENDOR_WEBGL
+    if (param === 37445) {
+        return '%s'; // default in headless: Google Inc.
+    }
+    // UNMASKED_RENDERER_WEBGL
+    if (param === 37446) {
+        return '%s'; // default in headless: Google SwiftShader
+    }
+    return result
+}
+}
+
+// There's more than one WebGL rendering context
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext#Browser_compatibility
+// To find out the original values here: Object.getOwnPropertyDescriptors(WebGLRenderingContext.prototype.getParameter)
+const addProxy = (obj, propName) => {
+    utils.replaceWithProxy(obj, propName, getParameterProxyHandler)
+}
+// For whatever weird reason loops don't play nice with Object.defineProperty, here's the next best thing:
+addProxy(WebGLRenderingContext.prototype, 'getParameter')
+addProxy(WebGL2RenderingContext.prototype, 'getParameter')
+""" % (
+            webgl_data.get("unmaskedVendor"),
+            webgl_data.get("unmaskedRenderer"),
+        )
+        evaluateOnNewDocument(driver, js_code)
+        hairline_fix(driver)
+
+        # url = "https://bot.sannysoft.com/"
+        # url = "https://sh.webmanajemen.com/webgl-information/"
+        url = "https://pixelscan.net/"
+        # url = "https://webglreport.com/"
+        # url = "https://privacycheck.sec.lrz.de/active/fp_wg/fp_webgl.html"
+        # url = "https://abrahamjuliot.github.io/creepjs/"
+        driver.get(url)
+
+        time.sleep(120)
+    except Exception as e:
+        print(e)
+    finally:
+        if driver:
+            driver.close()
