60da14d89ec0904dc6097d7bcf2991593fb4b034
dimaslanjaka <dimaslanjaka@gmail.com>
2024-09-14 07:27:00 +0700

    feat(navigator_languages): languages string type support

--- selenium_stealth/navigator_languages.py
+++ selenium_stealth/navigator_languages.py
@@ -1,12 +1,22 @@
 from pathlib import Path
-from typing import List
+from typing import List, Union
 from .wrapper import evaluateOnNewDocument
 from selenium.webdriver import Chrome as Driver
 
 
-def navigator_languages(driver: Driver, languages: List[str], **kwargs) -> None:
+def navigator_languages(
+    driver: Driver, languages: Union[str, List[str]], **kwargs
+) -> None:
+    languages_list = []
+    if isinstance(languages, str):
+        # sample full string en-US,en;q=0.9
+        strip_semicolon = languages.split(";")[0]  # en-US,en
+        split_comma = strip_semicolon.split(",")
+        languages_list = split_comma
+    else:
+        languages_list = languages
     evaluateOnNewDocument(
         driver,
         Path(__file__).parent.joinpath("js/navigator.languages.js").read_text(),
-        languages,
+        languages_list,
     )
